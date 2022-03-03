7fc432a781242ad69329c341c493237ded5b37c1
CameronDavis88 <cammien88@gmail.com>
2022-03-03 16:23:40 -0700

    Set up most of Assessment component for the quiz side, not results.

--- src/components/Assessment/Assessment.js
+++ src/components/Assessment/Assessment.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Banner from '../Banner/Banner';
 import Question from './Question';
 import { leanTermsList, eightWastesList } from '../../data';
 
 const Assessment = ({ customList }) => {
-//Need to keep track of score in this component
+  const [leanTestView, setLeanTestView] = useState(false);
+  const [wastesTestView, setWastesTestView] = useState(false);
+  const [customTestView, setCustomTestView] = useState(false);
 
 
-const mappedTermsQuiz = leanTermsList.map((question) => {
+  //Need to keep track of score in this component
 
-});
+  const toLeanTest = () => {
+    setLeanTestView(true);
+    setCustomTestView(false);
+    setWastesTestView(false);
+  };
 
-const mappedWastesQuiz = eightWastesList.map((question) => {
-  
-});
+  const toCustomTest = () => {
+    setLeanTestView(false);
+    setCustomTestView(true);
+    setWastesTestView(false);
+  };
 
-const mappedCustomQuiz = customList.map((question) => {
-  
-});
+  const toWastesTest = () => {
+    setLeanTestView(false);
+    setCustomTestView(false);
+    setWastesTestView(true);
+  };
+
+  const submitTest = () => {
+    setLeanTestView(false);
+    setCustomTestView(false);
+    setWastesTestView(false);
+  }
+
+  const mappedLeanQuiz = leanTermsList.map((question) => {
+    return (
+      <Question key={question.id} id={question.id} term={question.term} correctAnswer={question.answer} list={leanTermsList} />
+    )
+  });
+
+  const mappedWastesQuiz = eightWastesList.map((question) => {
+    return (
+      <Question key={question.id} id={question.id} term={question.term} correctAnswer={question.answer} list={eightWastesList} />
+    )
+  });
+
+  const mappedCustomQuiz = customList.map((question) => {
+    return (
+      <Question key={question.id} id={question.id} term={question.term} correctAnswer={question.answer} list={customList} />
+    )
+  });
 
 
 
@@ -26,7 +60,17 @@ const mappedCustomQuiz = customList.map((question) => {
     <div>
       Assessment
       <Banner />
+      <section>
+        <h2 onClick={toLeanTest} >Lean Terminology</h2>
+        <h2 onClick={toWastesTest} >8 Wastes</h2>
+        <h2 onClick={toCustomTest} >Custom Terms</h2>
+      </section>
 
+      <section>
+        {leanTestView ? mappedLeanQuiz : <></> }
+        {wastesTestView ? mappedWastesQuiz : <></>}
+        {customTestView ? mappedCustomQuiz : <></>}
+      </section>
     </div>
   );
 };
---
+++ src/components/Assessment/Question.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Question = ({ id, term, correctAnswer, list }) => {
+
+
+
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default Question;
